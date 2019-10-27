eeb8885e2dbbfeb80d602f58c84cac0fb2ea9e2f
Joshua Cheng <joshua.bj.cheng@gmail.com>
2019-10-27 16:48:45 -0500

    Add more dropdowns and submit page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import SubmitForm from "./SubmitForm";
+import AddInfo from "./submit/AddInfo";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>
           AdmitMe
         </h1>
-        {/* <Button>
-          get stuff
-        </Button> */}
       </header>
 
-        <SubmitForm />
+		<Router>
+			<Route exact path="/" component={SubmitForm}/>
+			<Route exact path="/submit" component={AddInfo}/>
+		</Router>
+        {/* <SubmitForm /> */}
     </div>
   );
 }
